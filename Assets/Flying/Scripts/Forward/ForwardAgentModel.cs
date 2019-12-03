aa0ea6bdd55cad1bb73ab4baff978533f7d94774
Pandogrammer <pandogrammer@gmail.com>
2019-12-03 07:06:46 -0300

    Forward agent

--- Assets/Flying/Scripts/Forward/ForwardAgentModel.cs
+++ Assets/Flying/Scripts/Forward/ForwardAgentModel.cs
@@ -1,6 +1,6 @@
 using UnityEngine;
 
-public class ForwardAgentModel
+public struct ForwardAgentModel
 {
     public float speed;
     public Vector3 position;
--- Assets/Flying/Scripts/Forward/UnityForwardArea.cs
+++ Assets/Flying/Scripts/Forward/UnityForwardArea.cs
@@ -1,3 +1,4 @@
+using Flying.Scripts.Fly;
 using Flying.Scripts.Stepable;
 using UnityEngine;
 
@@ -7,9 +8,29 @@ public class UnityForwardArea : UnityStepableArea
     [SerializeField] private Transform target;
     [SerializeField] private float moveSpeed;
     public ForwardAgentModel agent { get; private set; }
+    private int step;
+    private UnityForwardAgent unityAgent;
+
+    public void Awake()
+    {
+        unityAgent = GetComponentInChildren<UnityForwardAgent>();
+        Initialize();
+    }
+
+    private void Initialize()
+    {
+        step = 0;
+        var initialPosition = spawnPosition.localPosition;
+        var targetPosition = target.localPosition;
+    }
+
 
     public override void Step(int maxSteps)
     {
-        throw new System.NotImplementedException();
+        step += 1;
+        var newPosition = unityAgent.transform.localPosition;
+        var newRotation = unityAgent.body.rotation.eulerAngles;
+        var newVelocity = unityAgent.body.velocity;
+        var newTargetPosition = target.localPosition;
     }
 }
