bea184e3bc28d7b96892bedd6cc598f6103ce1ec
Fernando Hermes <fernando.hermes@etermax.com>
2019-12-18 13:42:18 -0300

    UI mejor

--- Assets/Nolose/Scripts/PlayerUI.cs
+++ Assets/Nolose/Scripts/PlayerUI.cs
@@ -1,15 +1,20 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using GenericScripts.Stepable;
+using Nolose.Scripts;
 using TMPro;
 using UnityEngine;
 
 public class PlayerUI : MonoBehaviour
 {
     [SerializeField] private TextMeshProUGUI hp;
+    [SerializeField] private TextMeshProUGUI step;
     [SerializeField] private Collector collector;
+    [SerializeField] private CollectingArea area;
 
     void Update()
     {
-        hp.text = collector.hp.ToString("0.0000");
+        hp.text = "HP: "+collector.hp.ToString("0.0000");
+        step.text = "STEP: "+area.step;
     }
 }
